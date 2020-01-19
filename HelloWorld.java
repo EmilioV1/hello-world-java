f7b84140180e584e57cd4ac2e9e241fa3b98c4f2
Emilio Valladares <vallae.wumbo@gmail.com>
2020-01-19 13:10:42 -0800

    Pushing first java program to github.

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of HelloWorld here.
+ * 
+ * @author (Emilio Valladares) 
+ * @version (0.0.1)
+ */
+public class HelloWorld {
+    
+    public static void main (String args[]){
+  
+        System.out.println("Hello world my name is Emilio Valladares!");
+  
+    };
+}
